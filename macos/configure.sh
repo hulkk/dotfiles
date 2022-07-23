1ec569df3368bf921d119f957cc7970606bdcdf1
Timo <timo@hulkkonen.fi>
2022-07-23 19:41:25 +0300

    Add settings

--- macos/configure.sh
+++ macos/configure.sh
@@ -6,3 +6,20 @@ defaults write "com.apple.dock" "launchanim" '0'
 
 echo "Minimise windows using: Scale effect"
 defaults write "com.apple.dock" "mineffect" 'scale'
+
+echo "disable facetime calls"
+defaults write "com.apple.TelephonyUtilities" "relayCallingDisabled" '1'
+
+echo "Disable - Spotlight Show in Menu Bar"
+defaults delete "com.apple.Spotlight" "NSStatusItem Visible Item-0"
+
+echo "Press globe/fn to: Do Nothing"
+defaults write "com.apple.HIToolbox" "AppleFnUsageType" '0'
+
+echo "Disable - Use smart quotes and dashes"
+defaults write "Apple Global Domain" "NSAutomaticDashSubstitutionEnabled" '0'
+defaults write "Apple Global Domain" "NSAutomaticQuoteSubstitutionEnabled" '0'
+
+echo "Disable - Correct spelling automatically"
+defaults write "Apple Global Domain" "WebAutomaticSpellingCorrectionEnabled" '0'
+defaults write "Apple Global Domain" "NSAutomaticSpellingCorrectionEnabled" '0'
