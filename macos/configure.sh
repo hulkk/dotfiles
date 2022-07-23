55c5afdefaaf6561a4a20162bd390dd6f5a32ffb
Timo <timo@hulkkonen.fi>
2022-07-23 22:24:38 +0300

    add settings

--- macos/configure.sh
+++ macos/configure.sh
@@ -1,14 +1,14 @@
 echo "Disable - Show recent applications in Dock"
-defaults write "com.apple.dock" "show-recents" '0'
+defaults write "com.apple.dock" "show-recents" -bool false
 
 echo "Disable - Animate opening application icon"
-defaults write "com.apple.dock" "launchanim" '0'
+defaults write "com.apple.dock" "launchanim" -bool false
 
 echo "Minimise windows using: Scale effect"
 defaults write "com.apple.dock" "mineffect" 'scale'
 
-echo "disable facetime calls"
-defaults write "com.apple.TelephonyUtilities" "relayCallingDisabled" '1'
+echo "Disable - Facetime Calls From iPhone"
+defaults write "com.apple.TelephonyUtilities" "relayCallingDisabled" -bool true
 
 echo "Disable - Spotlight Show in Menu Bar"
 defaults delete "com.apple.Spotlight" "NSStatusItem Visible Item-0"
@@ -17,13 +17,26 @@ echo "Press globe/fn to: Do Nothing"
 defaults write "com.apple.HIToolbox" "AppleFnUsageType" '0'
 
 echo "Disable - Use smart quotes and dashes"
-defaults write "Apple Global Domain" "NSAutomaticDashSubstitutionEnabled" '0'
-defaults write "Apple Global Domain" "NSAutomaticQuoteSubstitutionEnabled" '0'
+defaults write "Apple Global Domain" "NSAutomaticDashSubstitutionEnabled" -bool false
+defaults write "Apple Global Domain" "NSAutomaticQuoteSubstitutionEnabled" -bool false
 
 echo "Disable - Correct spelling automatically"
-defaults write "Apple Global Domain" "WebAutomaticSpellingCorrectionEnabled" '0'
-defaults write "Apple Global Domain" "NSAutomaticSpellingCorrectionEnabled" '0'
+defaults write "Apple Global Domain" "WebAutomaticSpellingCorrectionEnabled" -bool false
+defaults write "Apple Global Domain" "NSAutomaticSpellingCorrectionEnabled" -bool false
 
 echo "Disable - Now Playing Show in Menu Bar"
 defaults write "com.apple.controlcenter" "NSStatusItem Visible NowPlaying" '0'
 
+echo "Show all filename extensions"
+defaults write "Apple Global Domain" "AppleShowAllExtensions" -bool true
+
+echo "Disable - Show warningn before emptying the Bin"
+defaults write "com.apple.finder" "WarnOnEmptyTrash" -bool false
+
+echo "Show Path Bar"
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "Show Status Bar"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "Done."
