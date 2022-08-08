042b3e647635cbcf07ac4f76ff696d2df6b8a87f
Timo <timo@hulkkonen.fi>
2022-08-08 23:08:04 +0300

    Add privacy.sh

---
+++ macos/privacy.sh
@@ -0,0 +1,17 @@
+echo "disable os analytics & improvements"
+# TODO
+
+echo "disable location services, system services"
+# TODO
+
+echo "Disable "Spotlight Suggestions" and "Bing Web Searches" in System Preferences > Spotlight > Search Results."
+# TODO
+
+echo "Safari also has a "Spotlight Suggestions" setting that is separate from Spotlight's "Spotlight Suggestions". This uses the same mechanism as Spotlight, and if left enabled, Safari will send a copy of all search queries to Apple."
+# TODO
+
+echo "Disable dictionary lookup: Trackpad -> Point & Click -> Look up & data detectors off"
+# TODO
+
+echo "disable crash reporter"
+# defaults write com.apple.CrashReporter DialogType none
