1f04a9c9f9fc5e9bbc30753b0fc4b3f13d79eb89
Timo <timo@hulkkonen.fi>
2022-08-08 23:30:23 +0300

    update contents

--- macos/privacy.sh
+++ macos/privacy.sh
@@ -1,14 +1,22 @@
-echo "disable os analytics & improvements"
-# TODO
+echo "Disable macOS Analytics & Improvements"
+echo "### manual task ###"
+echo "System Preferences >> Security & Privacy >> Privacy >> Analytics & Improvements >> uncheck everything\n"
 
-echo "disable location services, system services"
-# TODO
+echo "Disable macOS location services for Applications"
+echo "### manual task ###"
+echo "System Preferences >> Security & Privacy >> Location Services >> uncheck every application\n"
 
-echo "Disable "Spotlight Suggestions" and "Bing Web Searches" in System Preferences > Spotlight > Search Results."
-# TODO
+echo "Disable macOS location services for System Services"
+echo "### manual task ###"
+echo "System Preferences >> Security & Privacy >> Location Services >> System Services, Details... >> uncheck all except Find My Mac and Time Zone & System Customisation\n"
 
-echo "Safari also has a "Spotlight Suggestions" setting that is separate from Spotlight's "Spotlight Suggestions". This uses the same mechanism as Spotlight, and if left enabled, Safari will send a copy of all search queries to Apple."
-# TODO
+echo "Disable Siri Suggestions"
+echo "### manual task ###"
+echo "System Preferences >> Spotlight >> Search Results >> uncheck Siri Suggestions\n"
+
+echo "Disable Safari Suggestions"
+echo "### manual task ###"
+echo "Safari >> Preferences >> Search >> uncheck Include Safari Suggestions\n"
 
 echo "Disable dictionary lookup: Trackpad -> Point & Click -> Look up & data detectors off"
 # TODO
