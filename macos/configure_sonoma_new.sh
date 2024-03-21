b3e8954475d6f67077b1de9167e6293e9bdbde73
Timo <timo@hulkkonen.fi>
2024-03-21 21:56:18 +0200

    update

--- macos/configure_sonoma_new.sh
+++ macos/configure_sonoma_new.sh
@@ -5,3 +5,6 @@ defaults write com.apple.screencapture show-thumbnail -bool false
 
 # configure dock icon size
 defaults write com.apple.dock tilesize -integer 40 && killall Dock
+
+# disable recent apps from dock
+defaults write com.apple.dock show-recents -bool false && killall Dock
