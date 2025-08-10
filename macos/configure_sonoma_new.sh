cd7c16f1eee29fdd184d22b1e007d385a2f407c3
Timo <timo@hulkkonen.fi>
2025-08-10 23:09:55 +0300

    Add Sonoma notes (#25)

---
+++ macos/configure_sonoma_new.sh
@@ -0,0 +1,13 @@
+# configure screenshots
+mkdir -p ~/screenshots
+defaults write com.apple.screencapture location ~/screenshots
+defaults write com.apple.screencapture show-thumbnail -bool false
+
+# configure dock icon size
+defaults write com.apple.dock tilesize -integer 40 && killall Dock
+
+# disable recent apps from dock
+defaults write com.apple.dock show-recents -bool false && killall Dock
+
+ # set accent color to red
+defaults write -g AppleAccentColor -integer 0
